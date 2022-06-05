6e04a59d2fa9a49ce626f7119e116ed6e22b717d
alvaro2302 <alvaro230297@hotmail.com>
2022-06-05 12:17:14 -0400

    Initial Commit

---
+++ GameStream/GameStreamApp.swift
@@ -0,0 +1,17 @@
+//
+//  GameStreamApp.swift
+//  GameStream
+//
+//  Created by Alvaro Cuiza on 5/6/22.
+//
+
+import SwiftUI
+
+@main
+struct GameStreamApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
